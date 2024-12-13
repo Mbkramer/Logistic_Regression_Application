2aaf07cc069e34a3f7298b3995ab42197b59a428
mbkramer <maxbkramers@gmail.com>
2024-12-13 16:10:26 -0600

    setting up

---
+++ logistic_regression_application.py
@@ -0,0 +1,17 @@
+# This is a practice application logistic regression and is heavily influenced by online resources
+# Not orginal work
+# Writtern by Max Kramer
+# Date: 12/13/24
+
+import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn import datasets
+
+
+
+def main():
+    print("Hello World!\n")
+
+if __name__=="__main__":
+    main()
